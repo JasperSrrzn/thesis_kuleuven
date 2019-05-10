ad108f3ca871d974187823ae88aa94a3a8a6a2a7
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-10 12:50:22 +0200

    update

--- Search/search.py
+++ Search/search.py
@@ -151,11 +151,11 @@ test_data = pd.read_csv('test_data_transe_100.csv')
 #test_data = test_data.drop_duplicates()
 
 no_rerank_results = pd.read_csv('results_no_rerank.csv')
-#pd_results = pd.read_csv('pd_results.csv')
-#results = list(pd_results['0'])
-results = []
+pd_results = pd.read_csv('results_transe_100.csv')
+results = list(pd_results['0'])
+#results = []
 no_rerank_results = list(no_rerank_results['no rerank'])
-for test_sample in range(0,len(test_data)):
+for test_sample in range(1880,len(test_data)):
     if no_rerank_results[test_sample]>30:
         try:
             print(test_sample)
