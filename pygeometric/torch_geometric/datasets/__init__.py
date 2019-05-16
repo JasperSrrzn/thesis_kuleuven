27e512156a5be6e9bc81f85aed54fb708fcd43c5
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-16 10:16:46 +0200

    new pygeometric

---
+++ pygeometric/torch_geometric/datasets/__init__.py
@@ -0,0 +1,49 @@
+from .karate import KarateClub
+from .tu_dataset import TUDataset
+from .planetoid import Planetoid
+from .cora_full import CoraFull
+from .coauthor import Coauthor
+from .amazon import Amazon
+from .ppi import PPI
+from .qm7 import QM7b
+from .qm9 import QM9
+from .entities import Entities
+from .mnist_superpixels import MNISTSuperpixels
+from .faust import FAUST
+from .dynamic_faust import DynamicFAUST
+from .shapenet import ShapeNet
+from .modelnet import ModelNet
+from .coma import CoMA
+from .shrec2016 import SHREC2016
+from .tosca import TOSCA
+from .pcpnet_dataset import PCPNetDataset
+from .geometry import GeometricShapes
+from .bitcoin_otc import BitcoinOTC
+from .icews import ICEWS18
+from .gdelt import GDELT
+
+__all__ = [
+    'KarateClub',
+    'TUDataset',
+    'Planetoid',
+    'CoraFull',
+    'Coauthor',
+    'Amazon',
+    'PPI',
+    'QM7b',
+    'QM9',
+    'Entities',
+    'MNISTSuperpixels',
+    'FAUST',
+    'DynamicFAUST',
+    'ShapeNet',
+    'ModelNet',
+    'CoMA',
+    'SHREC2016',
+    'TOSCA',
+    'PCPNetDataset',
+    'GeometricShapes',
+    'BitcoinOTC',
+    'ICEWS18',
+    'GDELT',
+]
