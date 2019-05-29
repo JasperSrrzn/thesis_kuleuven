89494992f20423b3a4fc4fc39039c3693b8e38bd
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-29 22:47:15 +0200

    update

--- Search/search.py
+++ Search/search.py
@@ -161,7 +161,7 @@ pd_results = pd.read_csv('results_gcn_200_new.csv')
 results = list(pd_results['0'])
 #results = []
 no_rerank_results = list(no_rerank_results['no rerank'])
-for test_sample in range(740,len(test_data)):
+for test_sample in range(1967,len(test_data)):
     if no_rerank_results[test_sample]>30:
         try:
             print('---------------')
